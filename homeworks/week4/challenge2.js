a3f4e96672cd9af74b3f77e13b56db66dbe3bf4b
Sharon <sharon@Sharon-MBP.local>
2019-06-03 13:20:06 +0800

    wk4Fixed

--- homeworks/week4/challenge2.js
+++ homeworks/week4/challenge2.js
@@ -3,105 +3,98 @@ const process = require('process');
 
 const clientID = 'br7rjl64ccvsnrsg4tzmyj597233pp';
 const url = 'https://api.twitch.tv/helix/';
+
 let gameID = '';
+let streams = '';
+let pagination = '';
 
-/* --- 使用 Promise --- */
-const getGameID = new Promise((resolve) => {
-  request(
-    {
-      url: `${url}games?name=${process.argv[2]}`,
-      headers: {
-        'Client-ID': clientID,
+const getGameID = () => {
+  const promise = new Promise((resolve, reject) => {
+    request(
+      {
+        url: `${url}games?name=${process.argv[2]}`,
+        headers: {
+          'Client-ID': clientID,
+        },
       },
-    },
-    (err, response, body) => {
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        try {
-          const result = JSON.parse(body);
-          gameID = result.data[0].id;
-          resolve(gameID);
-        } catch {
-          console.log('getGameID_資料有誤');
+      (err, response, body) => {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          try {
+            const result = JSON.parse(body);
+            gameID = result.data[0].id;
+            resolve();
+          } catch {
+            reject(new Error('getGameID_資料有誤'));
+          }
+        } else {
+          reject(new Error(`getGameID_statusCode:${response.statusCode}`));
         }
-      } else {
-        console.log('getGameID_statusCode:', response.statusCode);
-      }
-    },
-  );
-});
-
-getGameID.then((id) => {
-  // getStreamsByID
-  request(
-    {
-      url: `${url}streams?game_id=${id}&first=${20}`,
-      headers: {
-        'Client-ID': clientID,
       },
-    },
-    (err, response, body) => {
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        try {
-          const result = JSON.parse(body);
-          result.data.forEach((e) => {
-            console.log(e.id, e.title);
-          });
-        } catch {
-          console.log('getStreamsByID_資料有誤');
+    );
+  });
+  return promise;
+};
+
+const get100Streams = () => {
+  const promise = new Promise((resolve, reject) => {
+    request(
+      {
+        url: `${url}streams?game_id=${gameID}&first=${100}`,
+        headers: {
+          'Client-ID': clientID,
+        },
+      },
+      (err, response, body) => {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          try {
+            const result = JSON.parse(body);
+            streams = result.data;
+            pagination = result.pagination.cursor;
+            resolve(pagination);
+          } catch {
+            reject(new Error('get100Streams_資料有誤'));
+          }
+        } else {
+          reject(new Error(`get100Streams_statusCode:${response.statusCode}`));
         }
-      } else {
-        console.log('getStreamsByID_statusCode:', response.statusCode);
-      }
-    },
-  );
-});
+      },
+    );
+  });
+  return promise;
+};
 
+const get200Streams = () => {
+  const promise = new Promise((resolve, reject) => {
+    request(
+      {
+        url: `${url}streams?game_id=${gameID}&first=${100}&after=${pagination}`,
+        headers: {
+          'Client-ID': clientID,
+        },
+      },
+      (err, response, body) => {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          try {
+            const result = JSON.parse(body);
+            result.data.filter(e => streams.push(e));
+            streams.forEach((e, i) => {
+              console.log(i + 1, e.id, e.title);
+            });
+          } catch {
+            reject(new Error('get200Streams_資料有誤'));
+          }
+        } else {
+          reject(new Error(`get200Streams_statusCode:${response.statusCode}`));
+        }
+      },
+    );
+  });
+  return promise;
+};
 
-/* ------ 原始寫法：request 包在 request 裡面 ------
-request(
-  {
-    url: `https://api.twitch.tv/helix/games?name=${process.argv[2]}`,
-    headers: {
-      'Client-ID': clientID,
-    },
-  },
-  (err, response, body) => {
-    if (response.statusCode >= 200 && response.statusCode < 300) {
-      try {
-        const result = JSON.parse(body);
-        gameID = result.data[0].id;
-
-        // --- getStreamsByID start ----//
-          request(
-            {
-              url: `https://api.twitch.tv/helix/streams?game_id=${gameID}&first=${2}`,
-              headers: {
-               'Client-ID': clientID,
-              },
-            },
-            (err, response, body) => {
-              if (response.statusCode >= 200 && response.statusCode < 300) {
-                try {
-                  const result = JSON.parse(body);
-                  result.data.forEach((e) => {
-                    console.log(e.id , e.title);
-                  })
-                } catch {
-                  console.log('getStreamsByID_資料有誤');
-                }
-              } else {
-                console.log('getStreamsByID_statusCode:', response.statusCode);
-              }
-            }
-          )
-        // --- getStreamsByID end ----//
-
-      } catch {
-        console.log('getGameID_資料有誤');
-      }
-    } else {
-      console.log('getGameID_statusCode:', response.statusCode);
-    }
-  }
-)
-*/
+getGameID()
+  .then(get100Streams)
+  .then(get200Streams)
+  .catch((error) => {
+    console.log(error);
+  });
